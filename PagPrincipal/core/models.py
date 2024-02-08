538e1e785adcb9513433c3aef01ef043ac0317e3
“jaastorgar” <ja.astorgar@duocuc.cl>
2024-02-08 00:29:23 -0300

    conf. backend

--- PagPrincipal/core/models.py
+++ PagPrincipal/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    idCliente = models.IntegerField(primary_key=True, verbose_name='Id de categoria')
+    nombreCliente = models.CharField(max_length=50, verbose_name='Nombre del cliente')
+
+    def __str__(self):
+        return self.idCliente

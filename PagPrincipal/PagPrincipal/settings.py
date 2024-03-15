5f07547cd4e2c73862b09dcd32b62502d5a5c8eb
“jaastorgar” <ja.astorgar@duocuc.cl>
2024-03-15 11:46:31 -0300

    Cambio de base de datos.

--- PagPrincipal/PagPrincipal/settings.py
+++ PagPrincipal/PagPrincipal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,7 +84,7 @@ WSGI_APPLICATION = 'PagPrincipal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ventasluces',
+        'NAME': 'fiestaluces',
         'USER': 'javito',
         'PASSWORD': 'pass',
         'HOST': '127.0.0.1',
